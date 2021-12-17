d32dca2b63ee171de9c2e7883da546721c8b3114
Narrwe <narrwep@gmail.com>
2021-12-17 10:03:19 -0500

    built addBookController to post to books collection

--- back-end-routes/bookshelfRoute.js
+++ back-end-routes/bookshelfRoute.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const getUserBooks = require('../controllers/getUserBooksController');
+const addBook = require('../controllers/addBookController');
 
+router.post('/:username/bookshelf/addbook', addBook);
 router.get('/:username/bookshelf', getUserBooks);
 
 module.exports = router;
--- controllers/addBookController.js
+++ controllers/addBookController.js
@@ -1,5 +1,12 @@
 const { BookColl } = require('../models');
 
-const addBook = async (req, res) => {};
+const addBook = async (req, res) => {
+    try {
+        let newBook = await BookColl.insertMany(req.body);
+        res.status(201).send(`Successfully inserted ${newBook}`);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    };
+};
 
 module.exports = addBook;
