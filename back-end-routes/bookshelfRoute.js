b56fe77d68c16858b5af72bb19687f4832b81680
Narrwe <narrwep@gmail.com>
2021-12-17 10:52:46 -0500

    built deleteBookController to delete first book in books collection array

--- back-end-routes/bookshelfRoute.js
+++ back-end-routes/bookshelfRoute.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const getUserBooks = require('../controllers/getUserBooksController');
 const addBook = require('../controllers/addBookController');
+const deleteBook = require('../controllers/deleteBookController');
 
 router.post('/:username/bookshelf/addbook', addBook);
+router.delete('/:username/bookshelf/deletebook', deleteBook);
 router.get('/:username/bookshelf', getUserBooks);
 
 module.exports = router;
---
+++ controllers/deleteBookController.js
@@ -0,0 +1,12 @@
+const { BookColl } = require('../models');
+
+const deleteBook = async (req, res) => {
+    try {
+        let deletedBook = await BookColl.deleteOne({});
+        res.status(200).json(deletedBook);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    };
+};
+
+module.exports = deleteBook;
