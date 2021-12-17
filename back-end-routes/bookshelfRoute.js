efa2cd6037abbf32884f071a3257d9e1bd23054c
Narrwe <narrwep@gmail.com>
2021-12-17 12:28:27 -0500

    built editBookController to update book docs

--- back-end-routes/bookshelfRoute.js
+++ back-end-routes/bookshelfRoute.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const getUserBooks = require('../controllers/getUserBooksController');
 const addBook = require('../controllers/addBookController');
 const deleteBook = require('../controllers/deleteBookController');
+const editBook = require('../controllers/editBookController');
 
 router.post('/:username/bookshelf/addbook', addBook);
 router.delete('/:username/bookshelf/deletebook', deleteBook);
+router.put('/:username/bookshelf/editbook', editBook);
 router.get('/:username/bookshelf', getUserBooks);
 
 module.exports = router;
---
+++ controllers/editBookController.js
@@ -0,0 +1,12 @@
+const { BookColl } = require('../models');
+
+const editBook = async (req, res) => {
+    try {
+        let edit = await BookColl.findOneAndUpdate({}, req.body, { new: true });
+        res.status(200).json(edit);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = editBook;
