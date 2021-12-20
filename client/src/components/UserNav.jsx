e5d046d6dc7b2698912f5ad5cd3ba65e4e19407b
Narrwe <narrwep@gmail.com>
2021-12-19 22:04:42 -0500

    username can be passed to NewBook and Bookshelf through props

--- client/src/components/UserNav.jsx
+++ client/src/components/UserNav.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export default function UserNav(props) {
     return (
         <div className='userNavBar'>
-            <Link to={'/newbook'} className='navLink'>New Book</Link>
-            <Link to={'/:username/bookshelf'} className='navLink'>Bookshelf</Link>
+            <Link to={`/${props.username}/newbook`} className='navLink'>New Book</Link>
+            <Link to={`/${props.username}/bookshelf`} className='navLink'>Bookshelf</Link>
             <p>Hello, {props.username}!</p>
             <Link to={'/'} className='logout'>Logout</Link>
         </div>
-    )
-}
+    );
+};
