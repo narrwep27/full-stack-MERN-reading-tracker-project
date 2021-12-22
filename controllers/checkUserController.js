352f0a56d6393c265ad84d1aa133ba7dd45e2209
Narrwe <narrwep@gmail.com>
2021-12-22 13:19:24 -0500

    removed previous commit changes

--- controllers/checkUserController.js
+++ controllers/checkUserController.js
@@ -3,10 +3,8 @@ const { UserColl } = require('../models');
 const checkUser = async (req, res) => {
     try {
         let loginUser = await UserColl.findOne({ username: req.params.username });
-        console.log('error1');
         res.status(200).json(loginUser);
     } catch (error) {
-        console.log('error1');
         return res.status(500).json({ error: error.message });
     };
 };
