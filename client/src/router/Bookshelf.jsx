c86d5991a62ca23deeb520d656e045e455efc7a6
Narrwe <narrwep@gmail.com>
2021-12-23 02:25:07 -0500

    fixed deleteBook rendering bug so that books are correctly rendered after deletion

--- client/src/router/Bookshelf.jsx
+++ client/src/router/Bookshelf.jsx
@@ -7,27 +7,22 @@ import { BASE_URL } from '../globals';
 export default function Bookshelf(props) {
     const [userBooks, setUserBooks] = useState([]);
 
-    // const getUser = async () => {
-    //     let response = await axios.get(`${BASE_URL}existinguser/${props.match.params.username}`);
-    //     setUserBooks(response.data.books);
-    // };
     const getUserBooks = async () => {
         let response = await axios.get(`${BASE_URL}${props.match.params.username}/bookshelf`);
         setUserBooks(response.data.books);
     };
     const deleteBook = async (param) => {
-        let deletedBook = await axios.delete(`${BASE_URL}${props.match.params.username}/bookshelf/deletebook/${param}`);
-        let newBooksArr = userBooks.filter((item) => { return item !== deletedBook.data.value._id});
-        setUserBooks(newBooksArr);
-        await axios.put(`${BASE_URL}${props.match.params.username}/adduserbook`, 
+        let newBookIds = userBooks.map((book) => {return book._id}).filter((bookId) => {return bookId !== param._id});
+        let updatedUser = await axios.put(`${BASE_URL}${props.match.params.username}/adduserbook`, 
             {
-                books: [...newBooksArr]
+                books: [...newBookIds]
             }
         );
+        setUserBooks(updatedUser.data.books);
+        await axios.delete(`${BASE_URL}${props.match.params.username}/bookshelf/deletebook/${param._id}`);
     };
 
     useEffect(() => {
-        // getUser();
         getUserBooks();
     }, []);
 
--- controllers/addUserBookController.js
+++ controllers/addUserBookController.js
@@ -2,7 +2,7 @@ const { UserColl } = require('../models');
 
 const addUserBook = async (req, res) => {
     try {
-        let user = await UserColl.findOneAndUpdate({ username: req.params.username }, req.body, { new: true });;
+        let user = await UserColl.findOneAndUpdate({ username: req.params.username }, req.body, { new: true }).populate('books');;
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ error: error.message });
